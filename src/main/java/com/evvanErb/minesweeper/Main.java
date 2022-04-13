8e6ac9da6bdb4e09cc0c1296b13b99c1773c2d04
evvanErb <evvan@protonmail.com>
2022-04-13 13:02:41 -0400

    created basic build process

--- src/main/java/com/evvanErb/minesweeper/Main.java
+++ src/main/java/com/evvanErb/minesweeper/Main.java
@@ -1,10 +1,14 @@
 package com.evvanErb.minesweeper;
 import com.evvanErb.minesweeper.view.terminalview.TerminalView;
+import com.evvanErb.minesweeper.view.javafxview.JavaFXView;
 
 public class Main {
 
     public static void main(String[] args) {
-        TerminalView terminalView = new TerminalView();
-        terminalView.main();
+        //TerminalView terminalView = new TerminalView();
+        //terminalView.main();
+
+        JavaFXView javaFXView = new JavaFXView();
+        javaFXView.main(new String[] {});
     }
 }
